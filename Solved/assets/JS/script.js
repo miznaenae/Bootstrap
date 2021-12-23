032bf8aac334802ec4e80846644798494c4f68ac
miznaenae <renee.duff@gmail.com>
2021-12-23 06:02:18 -0500

    additional files added

--- Solved/assets/JS/script.js
+++ Solved/assets/JS/script.js
@@ -1,10 +1,14 @@
 let firstbox;
+let newBorder;
 
 allColors = ["red", "green", "blue", "yellow", "purple", "brown", "gold", "orange", "teal"];
+allBorders = ["dotted", "solid", "groove", "double", "inset", "outset", "ridge"];
 
 function changeButton() {
-
     firstbox = document.getElementById("first");
     firstbox.style.background = allColors[Math.floor(Math.random() * allColors.length)];
-    firstbox.style.border = "dotted 2px maroon"
+    newBorder = allBorders[Math.floor(Math.random() * allBorders.length)] + "2px" +
+        allColors[Math.floor(Math.random() * allColors.length)]
+    firstbox.style.border = newBorder;
+
 }
